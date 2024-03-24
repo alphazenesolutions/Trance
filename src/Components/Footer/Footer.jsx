cf5e6b81f4f7e56920727010cadfcb21b1ac612e
kowshik-7 <127116685+kowshik-7@users.noreply.github.com>
2024-03-24 18:08:57 +0530

    footer over

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,40 +8,42 @@ import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <div>
-      <div className="footer sectionWidth webHandler">
-        <div className="innerFooter">
-          <div className="foot1">
-            <h1>Visit us in Coimbatore.</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing.</p>
-            <p>Lorem Ipsum is simply dummy text of the printing.</p>
-            <p>Coimbatore 000 000.</p>
-          </div>
-          <div className="foot2">
-            <h1>Website Map</h1>
-            <div className="footGrid">
-              <p>Home</p>
-              <p>About Us</p>
-              <p>Blog</p>
-              <p>Contact Us</p>
+      <div className="">
+        <div className="footer sectionWidth ">
+          <div className="innerFooter">
+            <div className="foot1">
+              <h1>Visit us in Coimbatore.</h1>
+              <p>Lorem Ipsum is simply dummy text of the printing.</p>
+              <p>Lorem Ipsum is simply dummy text of the printing.</p>
+              <p>Coimbatore 000 000.</p>
+            </div>
+            <div className="foot2">
+              <h1>Website Map</h1>
+              <div className="footGrid">
+                <p>Home</p>
+                <p>About Us</p>
+                <p>Blog</p>
+                <p>Contact Us</p>
+              </div>
+            </div>
+            <div className="foot3">
+              <h1>Contact</h1>
+              <div className="footGrid">
+                <p>mail@trance.com</p>
+                <p>IN: +91 8096 272 2100</p>
+                <p>www.trance.com</p>
+                <p>Lorem ipsum</p>
+              </div>
+            </div>
+            <div className="foot4">
+              <FaFacebookF />
+              <FaYoutube />
+              <FaLinkedinIn />
+              <FaInstagram />
             </div>
           </div>
-          <div className="foot3">
-            <h1>Contact</h1>
-            <div className="footGrid">
-              <p>mail@trance.com</p>
-              <p>IN: +91 8096 272 2100</p>
-              <p>www.trance.com</p>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
-          <div className="foot4">
-            <FaFacebookF />
-            <FaYoutube />
-            <FaLinkedinIn />
-            <FaInstagram />
-          </div>
+          <h3>© All rights reserved</h3>
         </div>
-        <h3>© All rights reserved</h3>
       </div>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,6 +27,7 @@ const Home = () => {
         <HomeGrid />
         <TestimonialComp />
         <Diseases />
+        <GuideLine />
       </div>
     </div>
   );
